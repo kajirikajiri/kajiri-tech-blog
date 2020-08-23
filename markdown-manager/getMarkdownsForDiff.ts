f619289a3fd7cd61fadb7a17d76a8374c08574a8
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-23 17:24:03 +0900

    yarn commit

--- markdown-manager/getMarkdownsForDiff.ts
+++ markdown-manager/getMarkdownsForDiff.ts
@@ -1,7 +1,8 @@
 import {execSync} from 'child_process'
 
 export const getMarkdownsForDiff =(dir:string):string[]=>{
-  const getDateCmd = `git diff --staged --name-only ${dir} | head -c -1`
+  const getDateCmd = `git diff --staged --name-only '${dir}' | head -c -1`
+  console.log(getDateCmd)
   const result = execSync(getDateCmd).toString().split('\n')
   return result
 }
