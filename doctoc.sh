59d56bdc7aa6617b28d65e46b0e557b60a40a85a
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-23 17:39:55 +0900

    yarn commit

--- doctoc.sh
+++ doctoc.sh
@@ -3,9 +3,10 @@
 IFS='
 '
 
-a=($(git diff --name-only --staged))
+a=($(git diff --name-only --staged ./src/posts/*.md))
 for fileName in ${a[@]}; do
   if [ -e $fileName ]; then
-    ./node_modules/.bin/doctoc $fileName
+    trimed=$fileName | tr '\n' '\r' | sed 's,\s*\\\s*, ,g' | tr '\r' '\n'
+    ./node_modules/.bin/doctoc $trimed
   fi
 done
