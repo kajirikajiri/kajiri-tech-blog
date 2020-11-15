cb8067d95cb723bf186bf54a59afaebd3acbe1eb
kajirikajiri <kajirikajiri@gmail.com>
2020-11-15 19:15:06 +0900

    fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Layout = ({ location, title, children }) => {
+const Layout = ({ location, title, children }:{location:string,title:string,children:any}) => {
+
   const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
+    const isRootPath = location.pathname === rootPath
+
   let header
 
   if (isRootPath) {
