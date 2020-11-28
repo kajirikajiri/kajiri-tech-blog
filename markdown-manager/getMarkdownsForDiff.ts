956cdb0c48df26b857fc4d77095e0fb95c0dc152
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-11-28 20:24:30 +0900

    fix

--- markdown-manager/getMarkdownsForDiff.ts
+++ markdown-manager/getMarkdownsForDiff.ts
@@ -1,8 +1,9 @@
-import {execSync} from 'child_process'
+import { execSync } from "child_process"
 
-export const getMarkdownsForDiff =(dir:string):string[]=>{
-  const getDateCmd = `git diff --staged --name-only '${dir}' | head -c -1`
+export const getMarkdownsForDiff = (dir: string): string[] => {
+  const getDateCmd = `git diff --staged --name-only '${dir}'`
   console.log(getDateCmd)
-  const result = execSync(getDateCmd).toString().split('\n')
+  const result = execSync(getDateCmd).toString().split("\n")
+  result.pop()
   return result
 }
